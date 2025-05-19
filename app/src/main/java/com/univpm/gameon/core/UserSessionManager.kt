a8bcedce80c4d96a91ea430a2a033b23e428b42f
Guglielmo Borgognoni <guglielmo.borgognoni@grupposcai.it>
2025-05-19 14:02:37 +0200

    fun clear

--- app/src/main/java/com/univpm/gameon/core/UserSessionManager.kt
+++ app/src/main/java/com/univpm/gameon/core/UserSessionManager.kt
@@ -5,4 +5,9 @@ import com.univpm.gameon.data.collections.enums.UserRuolo
 object UserSessionManager {
     var isLoggedIn: Boolean = false
     var userRole: UserRuolo? = null
+
+    fun clear() {
+        isLoggedIn = false
+        userRole = null
+    }
 }
