10164ec8f94fb27a9600314186c3685765ebc328
Matteo <matteospernanzoni@gmail.com>
2025-05-21 17:08:57 +0200

    Aggironamento button CarteList.kt

--- app/src/main/java/com/univpm/gameon/ui/CarteList.kt
+++ app/src/main/java/com/univpm/gameon/ui/CarteList.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -95,8 +96,10 @@ fun CartaItem(carta: Carta, onDelete: () -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp),
-        elevation = CardDefaults.cardElevation(4.dp)
+            .padding(vertical = 4.dp)
+            .border(BorderStroke(2.dp, Color(0xFFCFFF5E)), shape = RoundedCornerShape(12.dp)),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFD3D3D3))
     ) {
         Row(
             modifier = Modifier
