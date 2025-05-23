91a86ca14b86c63b69fa6be48c584fe0fb530565
CactusGiack <giacomomarini23@gmail.com>
2025-05-23 12:01:35 +0200

    modifiche StrutturaForm.kt

--- app/src/main/java/com/univpm/gameon/ui/StrutturaForm.kt
+++ app/src/main/java/com/univpm/gameon/ui/StrutturaForm.kt
@@ -46,7 +46,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
